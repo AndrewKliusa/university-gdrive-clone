d92458e830c61222de32f865f7a593539f8d1388
AndrewKliusa <andriyklyusadev@gmail.com>
2026-05-22 10:15:28 +0200

    Added schema.sql to define the database strcture, also added ERD to /docs folder

---
+++ backend/database/schema.sql
@@ -0,0 +1,32 @@
+CREATE TABLE IF NOT EXISTS album (
+  id          INTEGER PRIMARY KEY AUTOINCREMENT,
+  name        TEXT    NOT NULL,
+  description TEXT,
+  created_at  TEXT    NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS person {
+  id          INTEGER PRIMARY KEY AUTOINCREMENT,
+  name        TEXT    NOT NULL,
+  created_at  TEXT    NOT NULL DEFAULT CURRENT_TIMESTAMP
+}
+
+CREATE TABLE IF NOT EXISTS photo (
+  id          INTEGER PRIMARY KEY AUTOINCREMENT,
+  album_id    INTEGER,
+  hash        TEXT    NOT NULL,
+  name        TEXT    NOT NULL,
+  size_bytes  INTEGER NOT NULL,
+  caption     TEXT,
+  taken_at    TEXT,
+  created_at  TEXT    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (album_id) REFERENCES album(id) ON DELETE SET NULL
+);
+
+CREATE TABLE IF NOT EXISTS photo_people (
+  photo_id   INTEGER NOT NULL,
+  person_id  INTEGER NOT NULL,
+  PRIMARY KEY (photo_id, person_id),
+  FOREIGN KEY (photo_id)  REFERENCES photo(id)  ON DELETE CASCADE,
+  FOREIGN KEY (person_id) REFERENCES person(id) ON DELETE CASCADE
+);
